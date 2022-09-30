7050d0b46548a0f4da9a724f31f35a71859fc897
jjsparkly <75410116+jjsparkly@users.noreply.github.com>
2022-09-29 21:04:15 -0700

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -1,7 +1,7 @@
 from socket import *
 HOST = "127.0.0.1"
 serverName = HOST
-serverPort = 12000
+serverPort = 6001
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input("Input lowercase sentence:")
 clientSocket.sendto(message.encode(),(serverName, serverPort))
