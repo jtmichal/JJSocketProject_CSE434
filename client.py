7e04d37b54b44cc5a781084b370c488f439ca1dc
Jacob Michalik <you@example.com>
2022-10-01 19:58:14 -0700

    additions of functions

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 from socket import *
-HOST = "10.120.70.117"
+HOST = "192.168.1.10"
 trackerName = HOST
 trackerPort = 6001
 clientSocket = socket(AF_INET, SOCK_DGRAM)
--- tracker.py
+++ tracker.py
@@ -1,9 +1,10 @@
 from socket import *
-HOST = "10.120.70.117"
+HOST = "192.168.1.10"
 trackerPort = 6001
 trackerSocket = socket(AF_INET, SOCK_DGRAM)
-trackerSocket.bind(("0.0.0.0", trackerPort))
+trackerSocket.bind((HOST, trackerPort))
 handle_list = []
+follower_list = []
 
 def capitalize(msg):
     cap_msg = msg.decode().upper()
@@ -22,10 +23,42 @@ def register(handle, HOST, trackerPort) -> str:
         info = "Success"
         return info
 
+def getHandles() -> str:
+    count = list.len()
+    list = handle_list
+    ans = ' '.join(list)
+    return ans
+
+def follow(follower, followee) -> str:
+    for x in follower_list:
+        if x == follower:
+            print("You're already following this user.")
+            info = "Failed"
+            return info
+    follower_list.append(follower)
+    follower_list.sort()
+    info = "Success"
+    return info
+
+def drop(follower, followee) -> str:
+    for x in follower_list:
+        if x == follower:
+            follower_list.remove(follower)
+            info = "Success"
+            return info
+    print("You don't follow this user")
+    info = "Failure"
+    return info
+
+
+
 
 print("The server is ready to receive")
 while True:
- message, clientAddress = trackerSocket.recvfrom(2048)
- modifiedMessage = register(message, "0.0.0.0", trackerPort)
- trackerSocket.sendto(modifiedMessage.encode(), clientAddress)
+    message, clientAddress = trackerSocket.recvfrom(2048)
+    modifiedMessage = register(message, "0.0.0.0", trackerPort)
+    trackerSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+
+ 
 
